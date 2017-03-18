f0d03c45bcac1e8e10c846a5c29b0a66fa42ea96
think4blink <think4blink@gmail.com>
2017-03-18 16:55:55 -0700

    + gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var sass = require('gulp-sass'); // сасс
 var gcmq = require('gulp-group-css-media-queries'); // объединяет @media
 var rigger = require('gulp-rigger'); //работа с инклюдами в html и js
 var rimraf = require('rimraf'); // ебенит всё к хуям
+var plumber = require('gulp-plumber'); // чтоб галп не вылетал
 var browserSync = require('browser-sync'); // недосервер
 var reload = browserSync.reload;
 
@@ -28,6 +29,7 @@ gulp.task('clean', function (cb) {
 
 gulp.task('sass', function() {
 	gulp.src('src/scss/[^_]*.scss')
+		.pipe(plumber())
 		.pipe(sass())
 		.pipe(gcmq())
 		.pipe(gulp.dest('dist/css'))
@@ -35,12 +37,14 @@ gulp.task('sass', function() {
 });
 gulp.task('html', function() {
 	gulp.src(['src/*.html', 'src/templates/*.html'])
+		.pipe(plumber())
 		.pipe(rigger())
 		.pipe(gulp.dest('dist'))
 		.pipe(reload({stream: true}));
 });
 gulp.task('js', function() {
 	gulp.src('src/js/*.js')
+		.pipe(plumber())
 		.pipe(rigger())
 		.pipe(gulp.dest('dist/js'))
 		.pipe(reload({stream: true}));
@@ -55,7 +59,7 @@ gulp.task('watch', function() {
 	gulp.watch('src/scss/[^_]*.scss', function() {
 		gulp.start('sass')
 	});
-	gulp.watch('src/**/*.html', function() {
+	gulp.watch(['src/**/*.html'], function() {
 		gulp.start('html')
 	});
 	gulp.watch('src/js/*.js', function() {
